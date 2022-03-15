e6ddab385222830030b412125e9ffd2b630c9098
keongmin <kmnam09@gmail.com>
2022-03-16 07:49:05 +0900

    edit and delete functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -37,6 +37,23 @@ exports.getEditProduct = (req, res, next) => {
   })
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updateTitle = req.body.title;
+  const updatePrice = req.body.price;
+  const updateImageUrl = req.body.imageUrl;
+  const updateDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updateTitle, 
+    updateImageUrl, 
+    updatePrice,
+    updateDesc
+  );
+  updatedProduct.save();
+  res.redirect('/products');
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
@@ -46,3 +63,7 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId
+}
